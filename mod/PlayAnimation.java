ad1817ac0a7bb20eed05ad8df3582d86530bf6a7
STR1KE6969 <64137481+OpenModDev@users.noreply.github.com>
2023-10-06 21:18:41 +0300

    Revert "Update PlayAnimation.java"
    This reverts commit 3eff7340697a3c25ed4a1f13e44ce300dc44caaa.

--- mod/PlayAnimation.java
+++ mod/PlayAnimation.java
@@ -13,14 +13,14 @@ import java.util.Map;
 
 public class PlayAnimation {
 <<<<<<< HEAD
+    public static void execute(IEventBus bus, Node node, RegistryObject<Item> itemObj) {
+=======
 <<<<<<< HEAD
     public static void execute(IEventBus bus, Node node, RegistryObject<Item> itemObj) {
 =======
     public static void execute(IEventBus buss, IEventBus sBus, Node node, RegistryObject<Item> itemObj) {
 >>>>>>> parent of bc5f1a1 (Update PlayAnimation.java)
-=======
-    public static void execute(IEventBus buss, IEventBus sBus, Node node, RegistryObject<Item> itemObj) {
->>>>>>> parent of bc5f1a1 (Update PlayAnimation.java)
+>>>>>>> parent of de5a754 (Update PlayAnimation.java)
         Map properties = (Map) node.data.get("properties");
         java.lang.String name = (java.lang.String) node.getInputData("Name <String>");
         AnimationState state = (AnimationState) node.getInputData("State <AnimationState>");
