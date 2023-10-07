abe872391cbf0b841fac1b329235056a438bde5a
STR1KE6969 <64137481+OpenModDev@users.noreply.github.com>
2023-10-07 11:05:12 +0300

    mob

---
+++ mod/OnAnimationInit_mob.java
@@ -0,0 +1,27 @@
+package dev.openmod.plugins.mod;
+
+import dev.openmod.project.Project;
+import dev.openmod.project.custom.CustomEvent;
+import dev.openmod.project.util.Node;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.RegistryObject;
+import software.bernie.geckolib.core.animation.AnimationState;
+
+public class OnAnimationInit_mob {
+    public static void execute(IEventBus bus, Node node, RegistryObject<Entity> entityObj) {
+        bus.addListener((event) ->{
+            if(event instanceof CustomEvent.AnimationInit) {
+                Item entity = entityObj.get();
+                if(!("entity." + Project.MODID + "." + ((CustomEvent.AnimationInit) event).getName()).equals(item.getName(new ItemStack(entity)).getString())) return;
+                AnimationState state = ((CustomEvent.AnimationInit) event).getAnimationState();
+                node.setOutputData("Mob <Entity>", entity);
+                node.setOutputData("State <AnimationState>", state);
+                node.TriggerNext("connector");
+            }
+        });
+    }
+}
