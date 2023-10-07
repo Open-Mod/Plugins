451c6db4b48a6f5fe53e568ad163c9ad8e8e8397
STR1KE6969 <64137481+OpenModDev@users.noreply.github.com>
2023-10-07 23:47:07 +0300

    mob

---
+++ mod/OnServerAiStep_mob.java
@@ -0,0 +1,23 @@
+package dev.openmod.plugins.mod;
+
+import dev.openmod.project.Project;
+import dev.openmod.project.custom.CustomEvent;
+import dev.openmod.project.util.Node;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.RegistryObject;
+
+public class OnServerAiStep_mob {
+    public static void execute(IEventBus bus, Node node, RegistryObject<EntityType> entityObj) {
+        bus.addListener((event) ->{
+            if(event instanceof CustomEvent.MobServerAiStep) {
+                EntityType entity = entityObj.get();
+                if(!("entity." + Project.MODID + "." + ((CustomEvent.MobGoalsInit) event).getName()).equals(entity.toString())) return;
+                Mob mob = ((CustomEvent.MobGoalsInit) event).getMob();
+                node.setOutputData("Mob <Mob>", mob);
+                node.TriggerNext("connector");
+            }
+        });
+    }
+}
