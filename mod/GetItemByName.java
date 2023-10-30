749b024d0963d9ad5c73a3c320d721fe9404b6dd
STR1KE6969 <64137481+OpenModDev@users.noreply.github.com>
2023-10-30 10:52:52 +0200

    fix

--- mod/GetItemByName.java
+++ mod/GetItemByName.java
@@ -9,18 +9,20 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class GetItemByName {
     public static void execute(IEventBus bus, Node node, RegistryObject itemObj) {
         java.lang.String name = (java.lang.String) node.getInputData("Name <String>");
         if(name == null) return;
-        Item item = null;
+        Supplier<Item> item = null;
         for(RegistryObject<Item> itemEntry : ItemInit.ITEMS.getEntries()) {
             if(name.equals(itemEntry.getKey().location().getPath())) {
-                item = itemEntry.get();
+                item = () -> itemEntry.get();
                 break;
             }
         }
-        if(item == null) item = RegistryObject.create(new ResourceLocation(name), ForgeRegistries.ITEMS).get();
+        if(item == null) item = () -> RegistryObject.create(new ResourceLocation(name), ForgeRegistries.ITEMS).get();
         node.setOutputData("Item <Item>", item);
     }
 }
