1ab7385f7110f945012241ffb6aabc29f5ec8f53
STR1KE6969 <64137481+OpenModDev@users.noreply.github.com>
2023-10-30 20:33:23 +0200

    movement

---
+++ mod/SetMovementController.java
@@ -0,0 +1,22 @@
+package dev.openmod.plugins.mod;
+
+import dev.openmod.project.custom.CustomMob;
+import dev.openmod.project.util.Node;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.RegistryObject;
+
+public class SetMovementController {
+    public static void execute(IEventBus bus, Node node, RegistryObject<EntityType> entityObj) {
+        Map properties = (Map) node.data.get("properties");
+        CustomMob mob = (CustomMob) node.getInputData("Mob <Mob>");
+        java.lang.String controller = (java.lang.String) properties.get("Controller");
+        if(controller.equals("Walking")) {
+            mob.setWalking();
+        } else if(controller.equals("Flying")) {
+            mob.setFlying();
+        }
+        node.TriggerNext("connector");
+    }
+}
