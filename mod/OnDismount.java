e5abbc1f31529e561ae857691294706fa199a3bf
STR1KE6969 <64137481+OpenModDev@users.noreply.github.com>
2023-10-31 14:31:02 +0200

    fix

--- mod/OnDismount.java
+++ mod/OnDismount.java
@@ -1,27 +1,32 @@
 package dev.openmod.plugins.mod;
 
 import dev.openmod.project.Project;
+import dev.openmod.project.custom.CustomMob;
 import dev.openmod.project.util.Node;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.EntityMountEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.RegistryObject;
 
-public class OnMount_mob {
+public class OnDismount {
     public static void execute(IEventBus bus, Node node, RegistryObject<EntityType> entityObj) {
         bus.addListener((event) -> {
             if(event instanceof EntityMountEvent) {
                 if(((EntityMountEvent) event).isDismounting()) {
                     EntityType entity = entityObj.get();
-                    Mob result = ((EntityMountEvent) event).getEntityBeingMounted();
-                    Entity player = ((EntityMountEvent) event).getEntityMounting();
+                    Entity result = ((EntityMountEvent) event).getEntityBeingMounted();
+                    Entity mounter = ((EntityMountEvent) event).getEntityMounting();
+                    CustomMob mob = (CustomMob) result.level().getEntity(result.getId());
+                    Player player = (Player) result.level().getEntity(mounter.getId());
                     if (!result.getName().getString().equals(entity.toString())) return;
-                    node.setOutputData("Target <Entity>", result);
-                    node.setOutputData("Entity <Entity>", player);
+                    node.setOutputData("Mob <Mob>", mob);
+                    node.setOutputData("Player <Player>", player);
                     node.TriggerNext("connector");
                 }
             }
